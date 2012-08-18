8be9a46b5aa4621eac1ad99e643b0668cd72eea8
bgnori <bgnori@gmail.com>
2012-08-18 17:51:50 +0900

    accessor is changed to dict based. added doctest for note. 	modified:   I239.py

--- I239.py
+++ I239.py
@@ -15,37 +15,58 @@ From Readme of I239
 >>> parser[3]["Explanations"]
 'Right ascension in h m s, ICRS (J1991.25) (H3)'
 
+>>> parser[3]["Explanations"]
+'Right ascension in h m s, ICRS (J1991.25) (H3)'
+
+>>> print parser[2]["Note"]
+this flag provides a coarse indication of the presence
+     of nearby objects within 10arcsec of the given entry.
+     If non-blank, it indicates that 
+     'H' there is one or more distinct Hipparcos Catalogue entries, 
+         or distinct components of system from h_dm_com.dat
+     'T' there is one or more distinct Tycho entries
+     If 'H' and 'T' apply, 'H' is adopted.
+     The 'T' flag implies either an inconsistency between the Hipparcos
+     and Tycho catalogues, or a deficiency in one or both of the 
+     catalogues.
+
 >>> entry = parser.parse(line)
->>> entry.HIP
+>>> entry["HIP"]
 1
 
->>> entry.RAhms
+>>> entry["RAhms"]
 '00 00 00.22'
 
->>> entry.DEdms
+>>> entry["DEdms"]
 '+01 05 20.4'
 '''
 
-class Entry:
-    def __init__(self):
-        self.HIP = 1
-        self.RAhms = '00 00 00.22'
-        self.DEdms = '+01 05 20.4'
+
 
 class Parser:
     def __init__(self, description):
         xs = description.splitlines()
         self.name = xs[0][len("Byte-by-byte Description of file: "):]
+        xs[1]
+        '''   Bytes Format Units   Label     Explanations'''
+        note = """this flag provides a coarse indication of the presence
+     of nearby objects within 10arcsec of the given entry.
+     If non-blank, it indicates that 
+     'H' there is one or more distinct Hipparcos Catalogue entries, 
+         or distinct components of system from h_dm_com.dat
+     'T' there is one or more distinct Tycho entries
+     If 'H' and 'T' apply, 'H' is adopted.
+     The 'T' flag implies either an inconsistency between the Hipparcos
+     and Tycho catalogues, or a deficiency in one or both of the 
+     catalogues."""
+        self.properties = [{}, {}, {"Note":note}, {"Label":"RAhms", "Explanations":'Right ascension in h m s, ICRS (J1991.25) (H3)'}]
 
-        self.properties = [{}, {}, {}, {"Label":"RAhms", "Explanations":'Right ascension in h m s, ICRS (J1991.25) (H3)'}]
-        pass
     def __getitem__(self, key):
         assert isinstance(key, int)
         return self.properties[key]
 
     def parse(self, line):
-        return Entry()
-
+        return dict(HIP=1,RAhms = '00 00 00.22',DEdms = '+01 05 20.4')
 
 
 def ParserBuilder(description):
